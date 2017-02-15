416bbc15bd418a59acd188f4f7387117ba53e601
slepeniotis <slepeniotis@gmail.com>
2017-02-16 00:40:56 +0200

    Inport The TaxiService project in github_15022017

---
+++ src/main/java/Taxi_Service/Evaluation.java
@@ -0,0 +1,81 @@
+package Taxi_Service;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author nyxteridas
+ * το ID πρέπει να είναι autoincrement
+ * αλλαγή των vectors σε λίστες
+ * date μόνο ως ημερομηνία
+ * 
+ * μέθοδος για εμφάνιση του evaluation
+ *
+ */
+
+public class Evaluation {
+	private int id;
+	private int rating;
+	private String comment;
+	private Date dateOfEval;
+	public TaxiDriver unnamed_TaxiDriver;
+	public Customer unnamed_Customer;
+
+	public Evaluation(int id, int rating, String comment) {
+		this.id = id;
+		this.rating = rating;
+		this.comment = comment;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getRating() {
+		return this.rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getComment() {
+		return this.comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	public Date getDateOfEval() {
+		return this.dateOfEval;
+	}
+
+	public void setDateOfEval(Date dateOfEval) {
+		this.dateOfEval = dateOfEval;
+	}
+
+	public void newEvaluation() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setUnnamed_TaxiDriver_(TaxiDriver unnamed_TaxiDriver) {
+		this.unnamed_TaxiDriver = unnamed_TaxiDriver;
+	}
+
+	public TaxiDriver getUnnamed_TaxiDriver() {
+		return this.unnamed_TaxiDriver;
+	}
+
+	public void setUnnamed_Customer_(Customer unnamed_Customer) {
+		this.unnamed_Customer = unnamed_Customer;
+	}
+
+	public Customer getUnnamed_Customer() {
+		return this.unnamed_Customer;
+	}
+}
---
+++ src/main/java/Taxi_Service/Taxi.java
@@ -0,0 +1,76 @@
+package Taxi_Service;
+
+import java.util.Vector;
+import java.util.Date;
+
+/**
+ * 
+ * @author nyxteridas
+ * το ID πρέπει να είναι autoincrement
+ * αλλαγή των vectors σε λίστες
+ * carmodeldate μόνο ημερομηνία
+ *  
+ *
+ */
+
+public class Taxi {
+	private int id;
+	private String carModel;
+	private String carType;
+	private String licensePlate;
+	private Date carModelDate;
+	public Vector<TaxiDriver> owns = new Vector<TaxiDriver>();
+	public Vector<Koursa> unnamed_Koursa = new Vector<Koursa>();
+
+	public Taxi(int id, String carModel, String carType, String licensePlate, Date carModelDate) {
+		this.id = id;
+		this.carModel = carModel;
+		this.carType = carType;
+		this.licensePlate = licensePlate;
+		this.carModelDate = carModelDate;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCarModel() {
+		return this.carModel;
+	}
+
+	public void setCarModel(String carModel) {
+		this.carModel = carModel;
+	}
+
+	public String getCarType() {
+		return this.carType;
+	}
+
+	public void setCarType(String carType) {
+		this.carType = carType;
+	}
+
+	public String getLicensePlate() {
+		return this.licensePlate;
+	}
+
+	public void setLicensePlate(String licensePlate) {
+		this.licensePlate = licensePlate;
+	}
+	
+	public Date getCarModelDate() {
+		return this.carModelDate;
+	}
+	
+	public void setCarModelDate(Date carModelDate) {
+		this.carModelDate = carModelDate;
+	}
+
+	protected void addTaxi() {
+		throw new UnsupportedOperationException();
+	}
+}
