96860200d6c7d2dbb045c8c34dc1f518c2087346
Nyxteridas <Nyxteridas@users.noreply.github.com>
2017-02-21 19:30:32 +0200

    dimioyrgia paketoy taxi.service gia na kaloyme apo ekei antikeimena/methodoys xoris anagki yparksis webservice

---
+++ src/main/java/taxi/service/TaxiService.java
@@ -0,0 +1,11 @@
+package taxi.service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import taxi.model.*;
+import taxi.persistence.JPAUtil;
+
+public class TaxiService {
+
+}
