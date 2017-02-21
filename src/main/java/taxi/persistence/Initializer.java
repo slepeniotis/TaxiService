76d1eeb744692fdcc39e53b874431c69b273d640
Nyxteridas <Nyxteridas@users.noreply.github.com>
2017-02-21 19:47:12 +0200

    more comments in initializer.java

--- src/main/java/taxi/persistence/Initializer.java
+++ src/main/java/taxi/persistence/Initializer.java
@@ -7,7 +7,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import taxi.model.*;
-import taxi.utils.AESEncrypt;
 
 import java.text.*;
 
@@ -16,86 +15,104 @@ import java.text.*;
 public class Initializer  {
 
 
-    /**
-     * Remove all data from database.
-     * The functionality must be executed within the bounds of a transaction
-     */
-    public static void eraseData() {
-        EntityManager em = JPAUtil.getCurrentEntityManager();
-       
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-        Query query = null;
+	/**
+	 * Remove all data from database.
+	 * The functionality must be executed within the bounds of a transaction
+	 */
+	public static void eraseData() {
+		EntityManager em = JPAUtil.getCurrentEntityManager();
 
-        query = em.createNativeQuery("delete from evaluation");
-        query.executeUpdate();
-        
-        query = em.createNativeQuery("delete from route");
-        query.executeUpdate();
-        
-        query = em.createNativeQuery("ALTER SEQUENCE hibernate_sequence RESTART WITH 1");
-        query.executeUpdate();
-        
-        tx.commit();
-        
-    }
-    
-  public static void prepareData() throws ParseException{
-	  
-	eraseData();  
-	
-	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	Date d;
-	try {
-		d = sdf.parse("21/12/2012");
-	}
-	catch (ParseException e){
-		d = sdf.parse("21/12/2012");
-	}
-	
-    Customer customer = new Customer("makis", "xristodoylopoylos", "gynaika", "mak", "mak", d, "location", "dfaggaadfadsfada", "fdaafdfa", 13671, "mastercard", 123456710, d, 123);
-    Customer customer2 = new Customer("mak", "xrist", "gynaika", "mak", "mak", d, "location", "dfaggaadfadsfada", "fdaafdfa", 13671, "mastercard", 123456710, d, 123);
-    customer2.setPassword("mak2");
-    Evaluation eval = new Evaluation(3, "djhalfhalcdalr", d);
-    Taxi taxi = new Taxi("dfadad", "fdafda", "347932", d, "fdafadfaea");
-    TaxiDriver taxidr = new TaxiDriver("makis", "xristodoylopoylos", "gynaika", "mak", "mak", d, "dfaggaadfadsfada", "fdaafdfa", 13671, "mastercard", 123456710, d, 123, taxi);
-    Route route = new Route("from", "to");
-    Request req = new Request(d, taxi, customer);
-    
-    /*Category cat = new Category();
-    Category cat2 = new Category();
-    cat.setDescription("Drama");
-    cat2.setDescription("Science Fiction");*/
-   
-    EntityManager em = JPAUtil.getCurrentEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    tx.begin();
-    
-    em.persist(customer);
-    em.persist(customer2);
-    em.persist(eval);
-    em.persist(req);
-    em.persist(route);
-    em.persist(taxi);
-    em.persist(taxidr);
-    
-    tx.commit();
-    
-    Query query = em.createQuery("select cust from Customer cust");
-    List<Customer> results = query.getResultList();
-    
-    for(Customer c : results) {
-        System.out.println(c.toString());
-    }   
+		//starting a new traksaction
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		Query query = null;
 
-}
+		//creating and executing several queries within the same transaction
+		query = em.createNativeQuery("delete from evaluation");
+		//execute the query given above
+		query.executeUpdate();
 
-public static void main (String [] args) {
-	try {
-		prepareData();
+		query = em.createNativeQuery("delete from route");
+		query.executeUpdate();
+
+		query = em.createNativeQuery("ALTER SEQUENCE hibernate_sequence RESTART WITH 1");
+		query.executeUpdate();
+
+		//save changes from this transaction to the DB
+		tx.commit();
+
 	}
-	catch (ParseException e){
-		System.out.println("poytana date");
+
+	public static void prepareData() throws ParseException{
+
+		//erasing data from the DB
+		eraseData();  
+
+		//creating a date for the scope of our testing
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date d;
+		try {
+			d = sdf.parse("21/12/2012");
+		}
+		catch (ParseException e){
+			d = sdf.parse("21/12/2012");
+		}
+		
+		//creating some objects of our model
+		//several objects of them, will need others to exist already
+		Customer customer = new Customer("makis", "xristodoylopoylos", "gynaika", "mak", "mak", d, "location", "dfaggaadfadsfada", "fdaafdfa", 13671, "mastercard", 123456710, d, 123);
+		Customer customer2 = new Customer("mak", "xrist", "gynaika", "mak", "mak", d, "location", "dfaggaadfadsfada", "fdaafdfa", 13671, "mastercard", 123456710, d, 123);
+		//changing the password of customer2 in order to check if it is encrypted correctly
+		customer2.setPassword("mak2");
+		Evaluation eval = new Evaluation(3, "djhalfhalcdalr", d);
+		Taxi taxi = new Taxi("dfadad", "fdafda", "347932", d, "fdafadfaea");
+		TaxiDriver taxidr = new TaxiDriver("makis", "xristodoylopoylos", "gynaika", "mak", "mak", d, "dfaggaadfadsfada", "fdaafdfa", 13671, "mastercard", 123456710, d, 123, taxi);
+		Route route = new Route("from", "to");
+		Request req = new Request(d, taxi, customer);
+
+		/*Category cat = new Category();
+		 * Category cat2 = new Category();
+		 * cat.setDescription("Drama");
+		 * cat2.setDescription("Science Fiction");
+		 */
+		
+		//start inserting the data (objects) that we have created previously, in the DB
+		//the way we are going to insert them is within a transaction
+		EntityManager em = JPAUtil.getCurrentEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		em.persist(customer);
+		em.persist(customer2);
+		em.persist(eval);
+		em.persist(req);
+		em.persist(route);
+		em.persist(taxi);
+		em.persist(taxidr);
+		
+		//the transaction is now committed
+		tx.commit();
+		
+		
+		//we are now preparing a query in order to see that the data are correctly inserted in the DB
+		Query query = em.createQuery("select cust from Customer cust");
+		//the result of the query is inserted in a list of results
+		List<Customer> results = query.getResultList();
+
+		//loop inside the list of results and run the method toString() of the class, in which the result of the query belongs
+		//in this case, the result list is of type Customer
+		for(Customer c : results) {
+			System.out.println(c.toString());
+		}   
+
 	}
-}
+
+	public static void main (String [] args) {
+		try {
+			prepareData();
+		}
+		catch (ParseException e){
+			System.out.println("poytana date");
+		}
+	}
 }
