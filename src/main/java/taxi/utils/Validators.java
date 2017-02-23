f74781303af144b347706a1a77a60f37adb27fce
Nyxteridas <Nyxteridas@users.noreply.github.com>
2017-02-24 01:00:15 +0200

    validation for duplicate emails

--- src/main/java/taxi/utils/Validators.java
+++ src/main/java/taxi/utils/Validators.java
@@ -14,80 +14,107 @@ import java.util.regex.Matcher;
 public class Validators {
 	public static boolean validateUsername(String username){
 		EntityManager em = JPAUtil.getCurrentEntityManager();
-		//starting a new transaction
-		EntityTransaction tx = em.getTransaction();
-		
-		StackTraceElement ste[] = Thread.currentThread().getStackTrace();        
-        if(ste[2].toString() == "Customer"){
-        	
-    		Query query = em.createQuery("select cust from Customer cust where username = :usrnm");
-    		query.setParameter("usrnm",username);  
-    		query.setMaxResults(1);
-    		List<Customer> results = query.getResultList();
-    		
-    		if(!results.isEmpty()){
-    			return false;
-    		}
-    		
-        }
-        else if(ste[2].toString() == "TaxiDriver"){
-        	Query query = em.createQuery("select taxdr from TaxiDriver taxdr where username = :usrnm");
-    		query.setParameter("usrnm",username);  
-    		query.setMaxResults(1);
-    		List<Customer> results = query.getResultList();
-    		
-    		if(!results.isEmpty()){
-    			return false;
-    		}
-        }
-        
-        return true;
-		
+
+		StackTraceElement ste[] = Thread.currentThread().getStackTrace();
+		if(ste[2].getClassName() == "taxi.model.Customer"){
+			
+			Query query = em.createQuery("select cust from Customer cust where username like :usrnm");
+			query.setParameter("usrnm", username); 
+			List<Customer> results = query.getResultList();
+
+			if(!results.isEmpty()){
+				return false;
+			}
+
+		}
+		else if(ste[2].getClassName() == "taxi.model.TaxiDriver"){
+			Query query = em.createQuery("select taxdr from TaxiDriver taxdr where username like :usrnm");
+			query.setParameter("usrnm", username);  
+			query.setMaxResults(1);
+			List<Customer> results = query.getResultList();
+
+			if(!results.isEmpty()){
+				return false;
+			}
+		}
+
+		return true;
+
 	}
-	
+
 	public static boolean validatePassword(String password){
 		final Pattern hasUppercase = Pattern.compile("[A-Z]");
 		final Pattern hasNumber = Pattern.compile("\\d");
-		
+
 		if (password.length() < 1 || password.length() < 8)
 			return false;
 		else if (!hasUppercase.matcher(password).find())
 			return false;
 		else if (!hasNumber.matcher(password).find())
-			return false;			
-    		
-    	return true;
+			return false;
+
+		return true;
 	}
-	
+
 	public static boolean validateEmail(String email){
 		String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-        Pattern p = Pattern.compile(ePattern);
-        Matcher m = p.matcher(email);
-        return m.matches();
-	}
-	
-	public static boolean validateCreditCard(String creditCardNumber, String expiryDate, String ccv){
+		Pattern p = Pattern.compile(ePattern);
+		Matcher m = p.matcher(email);
+		EntityManager em = JPAUtil.getCurrentEntityManager();
+		StackTraceElement ste[] = Thread.currentThread().getStackTrace(); 
 		
+		if(m.matches()){
+			if(ste[2].getClassName() == "taxi.model.Customer"){
+
+				Query query = em.createQuery("select cust from Customer cust where email = :eml");
+				query.setParameter("eml", email);  
+				query.setMaxResults(1);
+				List<Customer> results = query.getResultList();
+
+				if(!results.isEmpty()){
+					return false;
+				}
+
+			}
+			else if(ste[2].getClassName() == "taxi.model.TaxiDriver"){
+				Query query = em.createQuery("select taxdr from TaxiDriver taxdr where email = :eml");
+				query.setParameter("eml", email);  
+				query.setMaxResults(1);
+				List<Customer> results = query.getResultList();
+
+				if(!results.isEmpty()){
+					return false;
+				}
+			}
+			return true;
+		}
+
+		return false;
+
+	}
+
+	public static boolean validateCreditCard(String creditCardNumber, String expiryDate, String ccv){
+
 		if(creditCardNumber.length() < 16 || ccv.length() < 3)
 			return false;
-		
+
 		String month = expiryDate.substring(0, 2);
 		String year = expiryDate.substring(3, 5);
-		
+
 		Calendar now = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yy");
 		String curYear = sdf.format(Calendar.getInstance().getTime());
 		sdf = new SimpleDateFormat("MM");
 		String curMonth = sdf.format(Calendar.getInstance().getTime());
-		
+
 		if (Integer.parseInt(year) == Integer.parseInt(curYear))
 			if ((Integer.parseInt(month) > Integer.parseInt(curMonth)))
 				return true;
-		
+
 		if (Integer.parseInt(year) > Integer.parseInt(curYear))
 			return true;
-		
+
 		return false;
-		
+
 	}
 }
